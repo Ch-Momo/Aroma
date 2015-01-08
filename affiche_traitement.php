338f8839929d58533f7716be2cef0a34dfc9198a
AirChaos95 <usman.abid.butt@hotmail.com>
2015-01-08 21:49:55 +0100

    Revert "Ajout de tous vos fichiers."
    This reverts commit f1f0fd1c7d980d445481ce53e98a13fc56b2d7e7.

--- affiche_traitement.php
+++
@@ -1,7 +0,0 @@
-﻿
-<?php
-	$req = $bd->prepare('SELECT * FROM traitements;');
-	$req->execute();
-	while($res = $req->fetch(PDO::FETCH_ASSOC))
-		echo '<p> n:0'. $res['id_traitement'] . '  ' . $res['nom_traitement'] . '<br>' . $res['Desc_traitement'] . '</p>';
-?>
